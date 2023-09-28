a5740ce5c07421132185d1b349d305976595f8fa
Vivek <vivekraj78965.vr@gmail.com>
2023-09-28 21:10:00 +0530

    ReverseString(method 2)

--- String/ReverseString.js
+++ String/ReverseString.js
@@ -1,11 +1,23 @@
+// function reverseString(str) {
+//   let revStr = "";
+//   let len = str.length;
+//   for (let i = len - 1; i >= 0; i--) {
+//     // revStr = revStr + str.charAt(i);
+//     revStr = revStr + str[i];
+//   }
+//   return revStr;
+// }
+// const inputString = "ABCD"
+// console.log(reverseString(inputString));
+
+
+
+
+//using inbuilt method()
 function reverseString(str) {
-  let revStr = "";
-  let len = str.length;
-  for (let i = len - 1; i >= 0; i--) {
-    // revStr = revStr + str.charAt(i);
-    revStr = revStr + str[i];
-  }
-  return revStr;
+  let strArray = str.split(''); // converting the string to string array
+  strArray.reverse();
+  return strArray;
 }
 const inputString = "ABCD"
 console.log(reverseString(inputString));
