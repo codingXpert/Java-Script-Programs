3ac9c4623ad022c0f0b1d44bd1f04f2c585cdb10
Vivek <vivekraj78965.vr@gmail.com>
2023-09-30 19:20:13 +0530

    modified: print pairs

--- Basic/PairSum.js
+++ Basic/PairSum.js
@@ -4,11 +4,14 @@ function pairSum(arr,x) {
     let count = 0;
     for(let i=0; i<length;i++){
         for(let j=i+1;j<length;j++){
-            let pair = arr[i] + arr[j];
-            if(pair == targetSum)
-            count++;
+            let pairSum = arr[i] + arr[j];
+            if(pairSum == targetSum){
+                console.log( "(" ,arr[i], ":" ,arr[j], ")" );
+                count++;
+            }   
         }
     }
     return count;
 }
-console.log(pairSum([0,1,2,3,4,5,6],1));
+const result = pairSum([0,1,2,3,4,5,6],6)
+console.log("Target Sum Count: ", result);
